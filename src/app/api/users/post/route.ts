61ffaac189f1014ab18703431a73bc1bfa359118
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-11-13 10:39:27 -0500

    fix code

--- src/app/api/users/post/route.ts
+++ src/app/api/users/post/route.ts
@@ -5,14 +5,6 @@ const useRegisterService = new RegisterService()
 export async function POST(req: Request) {
     try {
         const formData = await req.formData();  
-        
-        // const body: IRegisterRequest = {
-        //     email: formData.get('email') as string,
-        //     password: formData.get('password') as string,
-        //     name: formData.get('name') as string,
-        //     role: formData.get('role') as string,
-        //     photo: formData.get('photo') as File | null,
-        // };
         const newUser = await useRegisterService.register(formData);
 
         return NextResponse.json(newUser, { status: 200 });
